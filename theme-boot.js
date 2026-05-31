6d56dfb73c9d1ad997d8d20945aec720564e7471
morggna <117986693+morggna@users.noreply.github.com>
2026-05-31 23:34:10 +0800

    Fix MV3 new tab CSP inline script

---
+++ theme-boot.js
@@ -0,0 +1,22 @@
+(function() {
+  try {
+    var localData = localStorage.getItem('newtabData');
+    var theme = localStorage.getItem('theme') || 'light';
+    var opL = 0.85;
+    var opD = 0.85;
+
+    if (localData) {
+      var d = JSON.parse(localData);
+      if (d.theme) theme = d.theme;
+      if (typeof d.opacityLight !== 'undefined') opL = d.opacityLight / 100;
+      if (typeof d.opacityDark !== 'undefined') opD = d.opacityDark / 100;
+    }
+
+    if (theme === 'dark') {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    }
+
+    document.documentElement.style.setProperty('--opacity-light', opL);
+    document.documentElement.style.setProperty('--opacity-dark', opD);
+  } catch (e) {}
+})();
